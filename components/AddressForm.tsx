9d83270f3022c95bdd3f9505b0a6bb98e6bdcc6b
Haris Ali <haris.aali9211@gmail.com>
2023-04-16 08:18:22 +0500

    done

--- components/AddressForm.tsx
+++ components/AddressForm.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, FormEvent, useState } from 'react';
 import styles from '../styles/AddressForm.module.css'
 
-function AddressForm(props: { handler: (address: string) => void }) {
+function  AddressForm(props: { handler: (address: string) => void }) {
 
   const [values, setValues] = useState({
     address: '',
